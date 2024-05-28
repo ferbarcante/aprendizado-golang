b007dae6ae4310c19573a1fbc1735a485074b7c1
Maria Fernanda Barçante <106846835+ferbarcante@users.noreply.github.com>
2024-05-28 12:34:38 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,9 @@
+//O arquivo main é o ponto de entrada pro programa GO
+//É preciso definir algumas coisas: precisa do pacote main, isso ja vai garantir que o arquivo pertence ao package principal do programa
+package main
+//uma regra de package nao pode ter na mesma pasta dois packages diferentes
+import "fmt"
+func main(){
+	//feita para iniciar o programa
+	fmt.Println("Hello World");
+}
